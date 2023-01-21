9db1699e20b203670ea1cd03f469974b3ddbb98a
Emerson P. de Souza <emersonline2007@gmail.com>
2023-01-21 00:37:29 -0300

    finalização do projeto

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -1,179 +1,135 @@
-<?php
+#criação do banco de dados
+# Criação da base de dados 
+CREATE database SKYSOLAR CHARACTER SET utf8 COLLATE UTF8_GENERAL_CI;
 
-namespace app\controllers;
+# selecionar db skysolar 
+USE SKYSOLAR;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+# Criação da tabela usuarios
+CREATE TABLE usuarios(
+	id int unsigned not null auto_increment,
+    nome_completo varchar(100) not null,
+	rg varchar(20), 
+    cpf varchar(20),
+	dt_nascimento date,
+    primary key(id)
+);
 
-use \app\DAO\MySQL\Skysolar\UsuariosDAO;
-use \app\Models\MySQL\Skysolar\UsuarioModel;
+# Criação da tabela enderecos fazendo referência à enderecos
+CREATE TABLE enderecos(
+	id int unsigned not null auto_increment,
+    cep varchar(20),
+	logradouro varchar(200),
+	bairro varchar(200),
+	cidade varchar(200),
+	estado varchar(200),
+	usuario_id int unsigned not null,
+    primary key(id),
+	CONSTRAINT fk_enderecos_usuario_usuario_id
+		FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+		ON DELETE CASCADE
+);
 
-use \app\DAO\MySQL\Skysolar\EnderecosDAO;
-use \app\Models\MySQL\Skysolar\EnderecoModel;
+# INSERÇÃO DE USUÁRIOS
+INSERT INTO usuarios(nome_completo, rg, cpf, dt_nascimento) 
+VALUE('Emerson Souza','155544447','85888788956', '1982-01-07');
+INSERT INTO usuarios(nome_completo, rg, cpf, dt_nascimento) 
+VALUE('Maria Souza','166644448','99888977958', '1960-04-01');
+INSERT INTO usuarios(nome_completo, rg, cpf, dt_nascimento) 
+VALUE('Welleson Gama','54445555','55888997854', '1992-03-15');
+INSERT INTO usuarios(nome_completo, rg, cpf, dt_nascimento) 
+VALUE('Paulo Gama','58888788','65444554888', '1950-11-10');
+INSERT INTO usuarios(nome_completo, rg, cpf, dt_nascimento) 
+VALUE('Emilly Araújo','15111444','55588597858', '2008-11-15');
 
-class UsuariosController extends Controller
-{
-    protected $post;
-    public function __construct()
-    {
-        $this->post = new UsuarioModel;
-    }
-    public function getList(Request $request, Response $response, array $args)
-    {
-        $usuariosDAO = new UsuariosDAO();
-        $usuarios = $usuariosDAO->getAllUsuarios();
-        $response = $response->withJson($usuarios);
+# Listando usuários
+SELECT * FROM usuarios;
 
-        $this->view('usuarioslist', ['users' => $usuarios, 'title' => 'Usuários']);
-    }
+#INSERÇÃO DE ENDEREÇOS
+#Usuário possui 2 endereços
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69028347','Rua Ademar de Barros, 20 - Sta Cruz','Flores', 'Manaus', 'AM', 1);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69028320','Rua 1, 12','Compensa', 'Manaus', 'AM', 1);
 
-    public function getCreateForm()
-    {
-        return $this->view('usuarioform', ['title' => 'Criar usuário']);
-    }
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua 3, 12','São José', 'Manaus', 'AM', 2);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua 7, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 2);
 
-    public function getCreate(Request $request)
-    {
-        $data = $request->getParsedBody();
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 3);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 3);
 
-        $usuarioDAO = new UsuariosDAO();
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 4);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 4);
 
-        $usuario = new UsuarioModel();
-        $usuario->setNome($data['nome'])
-            ->setRg($data['rg'])
-            ->setCpf($data['cpf'])
-            ->setNascimento($data['nascimento']);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 5);
+INSERT INTO enderecos(cep, logradouro, bairro, cidade, estado, usuario_id) 
+VALUES('69000000','Rua Barão de Indaiá, 124-A','Pq das Laranjeiras', 'Manaus', 'AM', 5);
 
-        $usuario = $usuarioDAO->insertUsuario($usuario);
+#Lista de endereços
+SELECT * 
+FROM enderecos;
 
-        // Array para armazenar os dois endereços
-        $endederecos = [
-            1 => [
-                'cep' => $data['cep'],
-                'logradouro' => $data['logradouro'],
-                'cidade' => $data['cidade'],
-                'estado' => $data['estado'],
-                'id_usuario' => $usuario,
-            ],
-            2 => [
-                'cep' => $data['cep2'],
-                'logradouro' => $data['logradouro2'],
-                'cidade' => $data['cidade2'],
-                'estado' => $data['estado2'],
-                'id_usuario' => $usuario,
-            ]
-        ];
 
-        $enderecoDAO = new EnderecosDAO();
+#Lista de enderecos por usuário
+SELECT
+	e.id,
+	u.nome_completo,
+    u.rg,
+    u.cpf,
+    u.dt_nascimento,
+    e.cep as cep,
+    e.logradouro as logradouro,
+    e.cidade as cidade,
+    e.estado as estado,
+    e.usuario_id as usuario
+FROM usuarios as u
+INNER JOIN enderecos as e on e.usuario_id = u.id
+WHERE
+	usuario_id = '1';
 
-        // Armazena endereço de cada vez, se não tiver campo preenchido nos ou em um deles endereço não será inserido
-        foreach ($endederecos as $end) {
-            if ($end['cep'] || $end['logradouro'] || $end['cidade']) {
+#ATUALIZAÇÃO DE REGISTROS
+#Atualização de endereco por usuário específico
+UPDATE enderecos
+SET 
+	cep = '69111333'
+WHERE
+	id = 2 AND
+    usuario_id = 1;
 
-                $endereco = new EnderecoModel();
+#Listando endereço alterado
+SELECT * FROM enderecos WHERE usuario_id = 1 AND id = 2;
 
-                $endereco->setCep($end['cep'])
-                    ->setLogradouro($end['logradouro'])
-                    ->setCidade($end['cidade'])
-                    ->setEstado($end['estado'])
-                    ->setUsuario_id($end['id_usuario']);
+#Atualizando usuarios
+UPDATE usuarios SET 
+		nome_completo = 'Paulo Gama',
+		rg = '44444556',
+		cpf = '447844574',
+		dt_nascimento = '1960-05-01' WHERE id=161;
 
-                $enderecoDAO->insertEndereco($endereco);
-            }
-        }
+#Atualizando enderecos
+UPDATE enderecos SET
+    cep = '695555', 
+    logradouro = 'Rua 2, 15 - Sta Cruz',
+    bairro = 'Flores',
+    cidade = 'Rio de Janeiro', 
+    estado = 'RJ' WHERE id = 42;
+    
+#Excluir usuário
+DELETE FROM usuarios
+WHERE id = 1;      
 
-        return $this->redirect("visualizar/{$end['id_usuario']}");
-    }
-
-    public function getView(Request $request, Response $response, array $args)
-    {
-        $usuariosDAO = new UsuariosDAO();
-        $enderecosDAO = new EnderecosDAO();
-
-        $usuario = $usuariosDAO->getUsuario($args['id']);
-
-        $enderecos = $enderecosDAO->getEndereco($args['id']);
-
-        return $this->view('usuarioview', ['user' => $usuario[0], 'enderecos' => $enderecos, 'title' => 'Usuário']);
-    }
-
-    public function getSearch(Request $request, Response $response, array $args)
-    {        
-        $data = $request->getParsedBody();
-        $usuariosDAO = new UsuariosDAO();
-        $usuarios = $usuariosDAO->getSearchUsuario($data['feildsearch']);
-        $response = $response->withJson($usuarios);
-
-        $this->view('usuarioslist', ['users' => $usuarios, 'title' => 'Usuários']);
-    }
-
-    public function geUpdateForm(Request $request, Response $response, array $args)
-    {
-        $usuarioDAO = new UsuariosDAO();
-
-        $usuariodata = $usuarioDAO->getUsuarioEndereco($args['id']);
-
-        $usuario = $usuariodata[0];
-
-        // dd($usuariodata);
-
-        return $this->view('usuarioupdateform', ['user' => $usuario, 'endereco' => $usuariodata, 'id' => $args['id']]);
-    }
-
-    public function getUpdate(Request $request, Response $response, array $args): Response
-    {
-        $data = $request->getParsedBody();
-
-        /** Atualização dos dados de usuários */
-        $usuarioDAO = new UsuariosDAO();
-
-        $usuario = [
-            'nome_completo' => $data['usuario_nome'],
-            'rg' => $data['usuario_rg'],
-            'cpf' => $data['usuario_cpf'],
-            'dt_nascimento' => $data['usuario_nascimento']
-        ];
-
-        /** Atualização dos dados de enderecos */
-        //Tratamento para carregar os endereços individualmente 
-        $endereco = [
-            0 => [
-                'id' => $data['id_endereco_0'],
-                'logradouro' => $data['logradouro_0'],
-                'cep' => $data['cep_0'],
-                'cidade' => $data['cidade_0'],
-                'estado' => $data['estado_0']
-            ],
-            1 => [
-                'id' => $data['id_endereco_1'],
-                'logradouro' => $data['logradouro_1'],
-                'cep' => $data['cep_1'],
-                'cidade' => $data['cidade_1'],
-                'estado' => $data['estado_1']
-            ]
-        ];
-
-        // Atualiazando usuário
-        $usuarioDAO->updatetUsuario($usuario, $data['id']);
-
-        $enderecoDAO = new EnderecosDAO();
-        foreach($endereco as $end){
-            if (!(empty($end['logradouro']) && empty($end['cep']) && empty($end['cidade']) && empty($end['estado']))) {
-                $enderecoDAO->updatetEndereco($end, $end['id']);
-            }
-        }
-
-        return $this->redirect('list');
-    }
-    public function getDelete(Request $request, Response $response, array $args)
-    {
-        $usuariosDAO = new UsuariosDAO();
-        $usuariosDAO->deleteUsuario($args['id']);
-
-        $response = $response->withJson([
-            'alert' => 'Usuário excluído com sucesso!'
-        ]);
-
-        return $this->view('home', []);
-    }
-}
+#Selecionando usuário específico - search
+SELECT
+	nome_completo,
+	rg,
+	cpf,
+	dt_nascimento
+	FROM usuarios WHERE
+	nome_completo like '%Emerson%';
